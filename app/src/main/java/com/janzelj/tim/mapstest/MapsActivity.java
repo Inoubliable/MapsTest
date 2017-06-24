bd299913b043396d8d668e101b9d391b18fb5398
Kosovircek <mitja.gracar94@gmail.com>
2017-06-24 23:24:32 +0200

    Crude implementation of fade-ing out of Custom Markers (as time pases and the free parking spots become increasingly more irellevant)

--- app/src/main/java/com/janzelj/tim/mapstest/MapsActivity.java
+++ app/src/main/java/com/janzelj/tim/mapstest/MapsActivity.java
@@ -1,16 +1,13 @@
 package com.janzelj.tim.mapstest;
 
-import android.Manifest;
-import android.app.ProgressDialog;
 import android.content.Context;
-import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.AsyncTask;
-import android.support.v4.app.ActivityCompat;
+import android.os.Handler;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -30,7 +27,6 @@ import com.google.gson.reflect.TypeToken;
 
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -43,6 +39,10 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.Map;
 
+
+//TODO(): implement time(from servers) for markers (now set value)
+//TODO(): after certian time(10min) old markers shold be delited
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
 
     private GoogleMap mMap;
@@ -52,36 +52,68 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     double lat;
     double lng;
 
-    String txtJson;
+
+    ArrayList<ParkingMarker> markersList;//stores ParkingMarkers for updating the oppacity of markers
+
+    Handler UI_HANDLER;//For thread events (setting a clocl trigerted fucntion for updating the oppacity of markers)
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
 
+
+        markersList = new ArrayList<>(); //sotres ParkingMarkers for updating the oppacity of markers
+
+
+        //Code to get user location
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-
         provider = locationManager.getBestProvider(new Criteria(), false);
-
         try {
             Location location = locationManager.getLastKnownLocation(provider);
-
             if(location != null) {
                 lat = location.getLatitude();
                 lng = location.getLongitude();
             }
-        } catch (SecurityException e) {
+        } catch (SecurityException e) {}
 
-        }
 
         //To trenutno dobi json iz linka in v Toast.show() izpiše podatke ki jih je vrnu server
         new JsonTask().execute("https://peaceful-taiga-88033.herokuapp.com/users");
+
+
+        UI_HANDLER = new Handler(); //to be handle thread events
+        UI_HANDLER.postDelayed(UI_UPDTAE_RUNNABLE, 1000);//This is like a clock triger event that runs on a UI(main) thread
+
+
+
     }
+
+
+    //This is like a clock triger event that runs on a UI(main) thread
+    //This is needed to update the oppacitiy of the markers, but they cannot be accest from another thread
+    //It gets called every second
+    Runnable UI_UPDTAE_RUNNABLE = new Runnable() {
+
+        @Override
+        public void run() {
+
+            for(ParkingMarker mark : markersList){
+
+                mark.updateAlpha();
+
+            }
+
+            UI_HANDLER.postDelayed(UI_UPDTAE_RUNNABLE, 1000);
+        }
+    };
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
@@ -91,7 +123,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     private void drawMyMarker() {
-        //Toast.makeText(this, "Lat: " + lat + ", Lng: " + lng, Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "Loading Free Parking Spaces", Toast.LENGTH_LONG).show();
 
         mMap.addPolygon(new PolygonOptions()
                 .add(
@@ -220,23 +252,25 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         protected void onPostExecute(String result) {
             super.onPostExecute(result);
 
-            Gson gson = new Gson();
-
-            txtJson = result;
+            Gson gson = new Gson();//a external libery object for reading HTTP JSON responses
 
             //Zbriše vse truntne markerje
             mMap.clear();
 
             try {
                 JSONArray tempArray = new JSONArray(result);//Paharsa string Json v Json array
+                Log.d("JSON arraqy : ",tempArray.toString());
 
                 for(int i=0; i<tempArray.length();i+=1){
 
+                    Log.d(i+"",tempArray.getString(i));
 
                     String innerArray = tempArray.getString(i);//ker je Json sestavljen iz arrayey morm dobit usak array posevi kot string
 
                     Type type = new TypeToken<Map<String, String>>(){}.getType(); //DA lagko pol v Map podam keksn tip je ker item
                     Map<String, String> myMap = gson.fromJson(innerArray, type); //Key-Value map k lagk pol vn uzamem lat, lng, time
+
+                    Log.d("Map"+i,myMap.toString());
 
                     double tempLat = Double.parseDouble(myMap.get("lat")); // najdem lat in za tem uzamem stevki in jih spremenim v double
                     double tempLng = Double.parseDouble(myMap.get("lng")); // najdem lng in za tem uzamem stevki in jih spremenim v double
@@ -244,21 +278,29 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
 
 
-                    //v Maps dodam nov marker
-                    mMap.addMarker(new MarkerOptions()
-                            .position(new LatLng(tempLat, tempLng))
-                            .title(tempName)
-                            .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
+                    //v Maps dodam nov marker in ga shranim v marker list
+                    markersList.add( new ParkingMarker( tempName, tempLat, tempLng,100f,10f,
+                                        mMap.addMarker(new MarkerOptions().position(new LatLng(tempLat, tempLng))
+                                                                        .title(tempName)
+                                                                        .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher_round))
+                                                                        .anchor(0.5f,0.5f))
+                                    )
+                    );
+
+                    //TODO(): camera should be moved to user location not last marker
                     //Kamero pomaknem na ta marker
                     mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(tempLat, tempLng), 3));
 
                 }
 
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
+            } catch (JSONException e) {e.printStackTrace();}
         }
 
 
     }
+
+
+
+
+
 }
---
+++ app/src/main/java/com/janzelj/tim/mapstest/ParkingMarker.java
@@ -0,0 +1,63 @@
+package com.janzelj.tim.mapstest;
+
+import com.google.android.gms.maps.model.Marker;
+
+/**
+ * Created by mitja on 6/24/17.
+ *
+ *
+ * This is a custom Marker class(basicly just has a Google Maps Marker class + time and id(from the database)+ added ability to handle touch events)
+ *
+ *
+ *
+ *
+ */
+
+public class ParkingMarker {
+
+
+    private Marker marker; //Google Maps api class for drawing markers on maps
+    double lat;
+    double lng;
+    private String id; //from the databse(to check the http return string for new parking spots)
+    private float precision; //if user uses GPS to annouce a free spot, we give(upon clicking the marker) a range in witch the viehcle is parked(GPS is not 100% accurate)
+    private float time; //then the spot was annouced free
+    private float alpha; //to graphicali show how recent is the free parking spot
+
+
+
+    public ParkingMarker(String id, double lat, double lng, float time, float precision, Marker marker){
+        this.id = id;
+        this.lat = lat;
+        this.lng = lng;
+        this.time = time;
+        this.precision = precision;
+
+        this.marker = marker; // marker is returned upon creating a marker with map.addMarker()
+        alpha = marker.getAlpha(); // could also just set it to 1
+    }
+
+
+
+    public void touch(){
+
+    }
+
+    public void updateAlpha(){
+        time += 1000; // potekla 1 sec
+        alpha = calculateAlphaForTime(time); //recalculate the oppacitiy of the marker- correspongind to time
+        marker.setAlpha(alpha); //set new oppacitiy
+    }
+
+    private float calculateAlphaForTime(float a){
+        //TODO(): update mathematical function
+        return alpha-0.1f; //this should be a more coplicated function involving TIME
+    }
+
+
+    public String getID(){
+        return id;
+    }
+
+
+}
